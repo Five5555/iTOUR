772a1fa8a1092f0e703f67638f5586f8715c1679
manoj9april <manoj9april@gmail.com>
2017-01-21 01:42:28 +0530

    Initial commit

---
+++ app/src/main/java/com/hackfest2017/itour/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.hackfest2017.itour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(WelcomeActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+
+
+
+}
