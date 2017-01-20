528584caab9a132199428d259eb2dea084976f26
manoj9april <manoj9april@gmail.com>
2017-01-21 03:05:42 +0530

    I ADDD COMMENT

--- app/src/main/java/com/hackfest2017/itour/SignIn.java
+++ app/src/main/java/com/hackfest2017/itour/SignIn.java
@@ -9,7 +9,7 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        //MANOJ
+        //MANOJhsagbkjh
 
     }
 }
