61e8dcbd064207315b2d49b3b3813b7930073f9f
rupeshkumar <rupeshkumar.behera1998@gmail.com>
2017-01-22 00:09:46 +0530

    initial commit

---
+++ app/src/main/java/com/weiwangcn/betterspinner/library/material/MaterialBetterSpinner.java
@@ -0,0 +1,19 @@
+package com.weiwangcn.betterspinner.library.material;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by RUPESH PC on 21-01-2017.
+ */
+public class MaterialBetterSpinner extends View {
+    public MaterialBetterSpinner(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAdapter(ArrayAdapter<String> arrayAdapter) {
+
+    }
+}
